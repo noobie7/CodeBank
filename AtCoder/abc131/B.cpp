ae646ae78b71c97411d37c642763a1b80b4b2876
noobie7 <mahendra060704@gmail.com>
2021-12-27 17:06:01 +0530

    Added Practice Solved ABC131

---
+++ AtCoder/abc131/B.cpp
@@ -0,0 +1,42 @@
+/*
+"An anomaly, I'm Muhammad Ali
+  Cause I know one day I'm gonna be the"
+                - Greatest, Eminem
+*/
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define ff first
+#define Shazam ios_base::sync_with_stdio(false);   cin.tie(NULL); cout.tie(NULL);
+#define ss second
+#define all(c) c.begin(),c.end()
+#define endl "\n"
+#define test() int t; cin>>t; while(t--)
+#define fl(i,a,b) for(int i = a ; i <b ;i++)
+#define get(a) fl(i,0,a.size())  cin>>a[i];
+#define pra(a) fl(i,0,a.size()) cout<<a[i]<<" ";	cout<<endl;
+#define pr(a,n) fl(i,0,n) cout<<a[i]<<" ";	cout<<endl;
+const ll INF = 2e18;
+const int inf = 2e9;
+const int mod1 = 1e9 + 7;
+
+
+
+int main(){
+    Shazam;
+    ll n, k; cin >> n >> k;
+    ll mn = INF;
+    ll mna = INF;
+    ll tot = 0;
+    for(int i = 1; i < n + 1; i++){
+        mn = min(mn, abs(k + i - 1));
+        if(mn == abs(k + i - 1)){
+            mna = k + i - 1;
+        }
+        tot += k + i - 1;
+    }
+    cout << tot - mna << endl;
+    return 0;
+}
