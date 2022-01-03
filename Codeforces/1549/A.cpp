b904ebbee987c44431da0672a0442d479982db29
noobie7 <mahendra060704@gmail.com>
2022-01-03 19:03:11 +0530

    Added VCsolved 1549

---
+++ Codeforces/1549/A.cpp
@@ -0,0 +1,48 @@
+/*
+"An anomaly, I'm Muhammad Ali
+  Cause I know one day I'm gonna be the"
+                - Greatest, Eminem
+*/
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define ff first
+#define Shazam ios_base::sync_with_stdio(false);   cin.tie(NULL); cout.tie(NULL);
+#define ss second
+#define all(c) c.begin(),c.end()
+#define endl "\n"
+#define test() int t; cin>>t; while(t--)
+#define fl(i,a,b) for(int i = a ; i <b ;i++)
+#define get(a) fl(i,0,a.size())  cin>>a[i];
+#define pra(a) fl(i,0,a.size()) cout<<a[i]<<" ";	cout<<endl;
+#define pr(a,n) fl(i,0,n) cout<<a[i]<<" ";	cout<<endl;
+const ll INF = 2e18;
+const int inf = 2e9;
+const int mod1 = 1e9 + 7;
+
+
+
+int main(){
+    Shazam;
+    test(){
+        int n; cin >> n;
+        if(n == 5){
+            cout << 2 << " " << 4 << endl;
+            continue;
+        }
+        for(int i = 1; i * i <= n; i++){
+            int rem = i;
+            int app = n - rem;
+            for(int j = i + 1; j * j < app; j++){
+                if(app % j == 0){
+                    cout << j << " " << app / j << endl;
+                    goto here;
+                }
+            }
+        }
+        here :;
+    }
+    return 0;
+}
