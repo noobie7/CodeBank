96eaabd2e07fe09be0fbdc87836460efc2929bf1
noobie7 <mahendra060704@gmail.com>
2022-01-16 14:38:06 +0530

    Added Contest Solved EXUN21B

---
+++ CodeChef/EXUN21B/SUMPARITY.cpp
@@ -0,0 +1,42 @@
+/*
+"An anomaly, I'm Muhammad Ali
+  Cause I know one day I'm gonna be the"
+                - Greatest, Eminem
+*/
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define ff first
+#define Shazam ios_base::sync_with_stdio(false);   cin.tie(NULL); cout.tie(NULL);
+#define ss second
+#define all(c) c.begin(),c.end()
+#define endl "\n"
+#define test() int t; cin>>t; while(t--)
+#define fl(i,a,b) for(int i = a ; i <b ;i++)
+#define get(a) fl(i,0,a.size())  cin>>a[i];
+#define pra(a) fl(i,0,a.size()) cout<<a[i]<<" ";	cout<<endl;
+#define pr(a,n) fl(i,0,n) cout<<a[i]<<" ";	cout<<endl;
+const ll INF = 2e18;
+const int inf = 2e9;
+const int mod1 = 1e9 + 7;
+
+
+
+int main(){
+    Shazam;
+    test(){
+        ll n, k; cin >> n >> k;
+        if(n == 1){
+            cout << ( k & 1 ? "Odd" : "Even") << endl;
+            continue;
+        }
+        if(k & 1){
+            cout << ( (n * k & 1) ? "Odd" : "Even") << endl;
+            continue;
+        }
+        cout << "Impossible" << endl;
+    }
+    return 0;
+}
