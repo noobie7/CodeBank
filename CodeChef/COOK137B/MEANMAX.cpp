663162cd8bea8e99454f930aa4cd75022a4a00f8
noobie7 <mahendra060704@gmail.com>
2022-01-23 22:40:14 +0530

    Added Contest Solved COOK137B

---
+++ CodeChef/COOK137B/MEANMAX.cpp
@@ -0,0 +1,37 @@
+/*
+    "An anomaly, I'm Muhammad Ali
+      Cause I know one day I'm gonna be the"
+                - Greatest, Eminem
+*/
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define Shazam ios_base::sync_with_stdio(false);   cin.tie(NULL); cout.tie(NULL);
+#define all(c) c.begin(),c.end()
+#define endl "\n"
+#define test() int t; cin>>t; while(t--)
+#define get(a) for(auto &i : a)  cin>>i;
+#define pra(a) for(auto i : a) cout<< i <<" ";	cout<<endl;
+const ll INF = 2e18;
+const int inf = 2e9;
+const int mod1 = 1e9 + 7;
+
+
+
+int main(){
+    Shazam;
+    test(){
+        int n; cin >> n;
+        vector<long double> a(n);
+        get(a);
+        sort(all(a));
+        long double s = 0;
+        for(int i = 0; i < n - 1; i++){
+            s += a[i];
+        }
+        cout << fixed << setprecision(7) << (s / (n - 1)) + a.back() << endl;
+    }
+    return 0;
+}
