cefabce5eb32deed8cd443cd0a0d8689659d0805
noobie7 <mahendra060704@gmail.com>
2022-01-26 13:06:49 +0530

    Added VC Solved 1604

---
+++ Codeforces/1604/B.cpp
@@ -0,0 +1,40 @@
+/*
+    "An anomaly, I'm Muhammad Ali
+      Cause I know one day I'm gonna be the"
+                - Greatest, Eminem
+*/
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define Shazam ios_base::sync_with_stdio(false);   cin.tie(NULL); cout.tie(NULL);
+#define all(c) c.begin(),c.end()
+#define endl "\n"
+#define test() int t; cin>>t; while(t--)
+#define get(a) for(auto &i : a)  cin>>i;
+#define pra(a) for(auto i : a) cout<< i <<" ";	cout<<endl;
+const ll INF = 2e18;
+const int inf = 2e9;
+const int mod1 = 1e9 + 7;
+
+
+
+int main(){
+    Shazam;
+    test(){
+        int n; cin >> n;
+        vector<int> a(n);
+        get(a);
+        if(n & 1 ^ 1){
+            cout << "Yes" << endl;
+            continue;
+        }
+        int ok = 0;
+        for(int i = 0; i + 1 < n; i++){
+            ok |= (a[i + 1] <= a[i]);
+        }
+        cout << ((ok) ? "Yes" : "No") << endl;
+    }
+    return 0;
+}
