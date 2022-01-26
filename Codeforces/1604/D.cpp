dbe1fa3fe189c2730b60b3c48fdb1c9a5738d190
noobie7 <mahendra060704@gmail.com>
2022-01-26 15:00:16 +0530

    Added Upsolved 1604

---
+++ Codeforces/1604/D.cpp
@@ -0,0 +1,35 @@
+/*
+    "An anomaly, I'm Muhammad Ali
+      Cause I know one day I'm gonna be the"
+                - Greatest, Eminem
+*/
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define Shazam ios_base::sync_with_stdio(false);   cin.tie(NULL); cout.tie(NULL);
+#define all(c) c.begin(),c.end()
+#define endl "\n"
+#define test() int t; cin>>t; while(t--)
+#define get(a) for(auto &i : a)  cin>>i;
+#define pra(a) for(auto i : a) cout<< i <<" ";	cout<<endl;
+const ll INF = 2e18;
+const int inf = 2e9;
+const int mod1 = 1e9 + 7;
+
+
+
+int main(){
+    Shazam;
+    test(){
+        int x, y; cin >> x >> y;
+        if(x > y){
+            cout << x + y << endl;
+        }
+        else{
+            cout << (y - (y % x) / 2) << endl;
+        }
+    }
+    return 0;
+}
