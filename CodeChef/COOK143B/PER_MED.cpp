966ef5a2c58de4211c2fffb024dbd6b5832a48b3
noobie7 <mahendra060704@gmail.com>
2022-07-04 00:30:32 +0530

    Added contest solved COOK143B

---
+++ CodeChef/COOK143B/PER_MED.cpp
@@ -0,0 +1,44 @@
+/*
+    "An anomaly, I'm Muhammad Ali
+    Cause I know one day I'm gonna be the"
+                - Greatest, Eminem
+*/
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define Shazam ios_base::sync_with_stdio(false);   cin.tie(NULL); cout.tie(NULL);
+#define all(c) c.begin(),c.end()
+#define endl "\n"
+#define test() int t; cin>>t; while(t--)
+#define get(a) for(auto &i : a)  cin>>i;
+#define pra(a) for(auto i : a) cout<< i <<" ";	cout<<endl;
+const ll INF = 2e18;
+const int inf = 2e9;
+const int mod1 = 1e9 + 7;
+
+
+
+int main(){
+    Shazam;
+    test(){
+        int n; cin >> n;
+        deque<int> dq(n);
+        iota(all(dq), 1);
+        int turn = 0;
+        while(dq.size() > 0){
+            if(turn & 1) {
+                cout << dq.front() << " ";
+                dq.pop_front();
+            }
+            else{
+                cout << dq.back() << " ";
+                dq.pop_back();
+            }
+            turn ^= 1;
+        }
+        cout << endl;
+    }
+    return 0;
+}
