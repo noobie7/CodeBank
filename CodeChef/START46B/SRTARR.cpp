9b66b214bd80044d221ebfea008c41d8f3c4414b
noobie7 <mahendra060704@gmail.com>
2022-07-07 01:15:18 +0530

    Added Contest Solved START46B

---
+++ CodeChef/START46B/SRTARR.cpp
@@ -0,0 +1,35 @@
+/*
+    "An anomaly, I'm Muhammad Ali
+    Cause I know one day I'm gonna be the"
+                - Greatest, Eminem
+*/
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define Shazam ios_base::sync_with_stdio(false);   cin.tie(NULL); cout.tie(NULL);
+#define all(c) c.begin(),c.end()
+#define endl "\n"
+#define test() int t; cin>>t; while(t--)
+#define get(a) for(auto &i : a)  cin>>i;
+#define pra(a) for(auto i : a) cout<< i <<" ";	cout<<endl;
+const ll INF = 2e18;
+const int inf = 2e9;
+const int mod1 = 1e9 + 7;
+
+
+
+int main(){
+    Shazam;
+    test(){
+        int n; cin >> n;
+        string s; cin >> s;
+        int ans = 0;
+        for(int i = 1; i < n; i++){
+            ans += (s[i] == '0' && s[i - 1] == '1');
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
